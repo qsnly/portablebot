32f4a75964a345f24d4cf6f933a2b9fe9c84ef76
Sashinovsky <87412223+sashinovsky@users.noreply.github.com>
2023-08-28 14:37:07 +0300

    upload and release
    finaly

---
+++ config.py
@@ -0,0 +1,35 @@
+token = 'your token here'
+
+min_random = 0
+max_random = 2
+random_result = 1
+
+# If 0 or other, whitelist will be enabled
+blacklist = 1
+
+news_channel = '@your news channel'
+admin_username = '@your username'
+admin_id = [your tg id]
+admins = [your tg id]
+logs_channel = ' enter here nothing'
+logs_channel_id = 'enter here nothing'
+activate_logs = False
+
+extensions_dir = 'Extensions/'
+locales_dir = 'Locales/'
+stats_dir = 'Stats/'
+users_dir = 'Users/'
+lists_dir = 'Lists/'
+bases_dir = 'Bases/'
+
+path_to_num_of_all_gen_msgs = 'Stats/num_of_all_gen_msgs.txt'
+
+path_to_base = 'Bases/botbase.txt'
+path_to_log = 'Logs/log_{time}.log'
+
+
+lists_files = ['chatslist', 'blacklist.txt', 'whitelist.txt', 'botbasechats.txt', 'disabledchats.txt', 'logsdisabledchats.txt']
+
+levels = [0, 15, 30, 50, 80, 100, 150, 1800, 3500]
+level_names = ['Начинающий', 'Взрослее котёнка', 'Человек', 'Добрый', 'Колокол', 'КОД', '■ - Картина', 'Ветеран чата', 'С самого начала...' ]
+
